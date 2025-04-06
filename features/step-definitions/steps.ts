96792524e10813e4813de44dc9588c03119156c9
dgroumpos <dngroumpos@hotmail.com>
2025-04-06 09:43:21 +0300

    fixed imports and removed unused code from steps

--- features/step-definitions/steps.ts
+++ features/step-definitions/steps.ts
@@ -1,25 +1,21 @@
 import { Given, Then, When } from '@wdio/cucumber-framework';
-import allure from '@wdio/allure-reporter';
 import HomePage from '../pageobjects/HomePage';
 import EventDetailsPage from '../pageobjects/EventDetailsPage';
-import { CustomWorld } from '../pageobjects/utility/world';
+import { CustomWorld } from '../pageobjects/utility/World';
 
 Given('Home page is displayed', async  () => {
     await HomePage.elementDisplayed(HomePage.kantoLocation);
 });
 
 When('I click on the Popular in Kanto link', async  () => {
-    await allure.addStep('Display Popular in Kanto');
     await HomePage.tapElement(HomePage.popularLink);
 });
 
 Then('the event details page is displayed', async  () => {
-    await allure.addStep('Event details page is displayed');
     await EventDetailsPage.elementDisplayed(EventDetailsPage.eventDetailsHeader);
 });
 
 Then('the additional section is displayed', async  () => {
-    await allure.addStep('Additional section is displayed');
     await EventDetailsPage.elementDisplayed(EventDetailsPage.battlesSection);
 });
 
